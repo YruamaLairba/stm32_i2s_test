eb7f227c72f159d114787cb69a8a0ef9b5a2f5dc
Yruama_Lairba <yruama_lairba@hotmail.com>
2022-06-12 11:28:24 +0200

    wip on transfer project

--- transfer/src/main.rs
+++ transfer/src/main.rs
@@ -12,46 +12,59 @@ use cortex_m_rt::entry;
 
 use rtt_target::{rprint, rprintln, rtt_init_print};
 
-use stm32_i2s_v12x::I2sPeripheral;
 use stm32_i2s_v12x::driver::{DataFormat, *};
 use stm32_i2s_v12x::marker;
 use stm32_i2s_v12x::transfer::*;
+use stm32_i2s_v12x::I2sPeripheral;
 use stm32f4xx_hal::i2s::stm32_i2s_v12x;
 
 use stm32f4xx_hal::gpio::NoPin;
 use stm32f4xx_hal::i2s::I2s;
 use stm32f4xx_hal::pac::CorePeripherals;
 use stm32f4xx_hal::pac::Peripherals;
+use stm32f4xx_hal::pac::{RCC, SPI2, SPI3, SPI5};
 use stm32f4xx_hal::prelude::*;
+use stm32f4xx_hal::rcc::Reset;
 
 macro_rules! test_32bits {
     ($i2s2_transfer:ident, $i2s3_transfer:ident) => {
-        let mut count = 0;
-        let mut smpls = [(0, 0); 3];
+        let mut rcount = 0;
+        let mut wcount = 0;
+        let mut smpls = [(0x88888888u32 as _ , 0x88888888u32 as _); 4];
         let frm = [
-            (0xDEADBEEF_u32 as _, 0xFEDEDBEE_u32 as _),
-            (0xCAFEC0CA_u32 as _, 0xFADEDFEE_u32 as _),
-            (0xBADC0C0A_u32 as _, 0xF00DC0DE_u32 as _),
+            (0x55555555_u32 as _, 0xFFFFFFFF_u32 as _),
+            (0x22222222_u32 as _, 0x33333333_u32 as _),
+            (0xAAAABBBB_u32 as _, 0xCCCCDDDD_u32 as _),
+            (0x44446666_u32 as _, 0x77779999_u32 as _),
         ];
-        $i2s2_transfer.begin();
-        $i2s3_transfer.begin();
-        while count < 3 {
-            $i2s2_transfer.write(frm[count]).ok();
+        loop {
+            if wcount <= 3 {
+                if $i2s2_transfer.write(frm[wcount]).is_ok() {
+                    wcount += 1;
+                }
+            } else {
+                $i2s2_transfer.write((0xC3C3C3C3u32 as _,0xC3C3C3C3u32 as _)).ok();
+            }
 
             if let Ok(s) = $i2s3_transfer.read() {
-                smpls[count] = s;
-                count += 1;
+                if rcount <= 3 {
+                    smpls[rcount] = s;
+                    rcount += 1;
+                } else {
+                    break
+                }
             }
         }
 
-        for (i,(e, s)) in frm.iter().zip(smpls.iter()).enumerate() {
+        for (i, (e, s)) in frm.iter().zip(smpls.iter()).enumerate() {
             let msg = if i == 0 {
                 "Ignored"
-            } else if e == s {
+            } else if *e == *s {
                 "Ok"
             } else {
                 "Fail"
             };
+            //rprintln!("{:?} {:x} {:x}, {:?} {:x} {:x}, {}", *e,e.0,e.1,*s,s.0,s.1,*e==*s);
             rprintln!(
                 "transmitted: {:08x} {:08x}, received: {:08x} {:08x}, {}",
                 e.0 as u32,
@@ -61,7 +74,7 @@ macro_rules! test_32bits {
                 msg
             );
         }
-    }
+    };
 }
 
 #[entry]
@@ -86,35 +99,70 @@ fn main() -> ! {
 
     // I2S pins: (WS, CK, MCLK, SD)
     let i2s2_pins = (gpiob.pb12, gpiob.pb13, gpioc.pc6, gpiob.pb15);
-    let mut i2s2 = Some(I2s::new(dp.SPI2, i2s2_pins, &clocks));
+    let mut i2s2 = I2s::new(dp.SPI2, i2s2_pins, &clocks);
 
     let i2s3_pins = (gpioa.pa4, gpioc.pc10, NoPin, gpioc.pc12);
-    let mut i2s3 = Some(I2s::new(dp.SPI3, i2s3_pins, &clocks));
+    let mut i2s3 = I2s::new(dp.SPI3, i2s3_pins, &clocks);
 
     let i2s5_pins = (gpiob.pb1, gpiob.pb0, NoPin, gpiob.pb8);
-    let mut i2s5 = Some(I2s::new(dp.SPI5, i2s5_pins, &clocks));
+    let mut i2s5 = I2s::new(dp.SPI5, i2s5_pins, &clocks);
 
-    let i2s2_transfer_conf = I2sTransferConfig::new_master()
-        .transmit()
+    let transfer_base_conf = I2sTransferConfig::new_master()
+        .receive()
         .standard(marker::Philips)
         .data_format(marker::Data32Channel32)
         .master_clock(true)
         .request_frequency(1); //try to get the lowest possible sample rate
-    let i2s3_transfer_conf = i2s2_transfer_conf.to_slave().receive();
-    let i2s5_driver_conf = I2sDriverConfig::new_master()
+    let driver_base_conf = I2sDriverConfig::new_master()
         .receive()
         .standard(Philips)
         .data_format(DataFormat::Data32Channel32)
         .master_clock(true)
         .request_frequency(1);
-        //.i2s_driver(i2s5.take().unwrap());
-
-    let mut i2s2_transfer = i2s2_transfer_conf.i2s_transfer(i2s2.take().unwrap());
-    let mut i2s3_transfer = i2s3_transfer_conf.i2s_transfer(i2s3.take().unwrap());
+    //.i2s_driver(i2s5.take().unwrap());
 
     rprintln!("master transmit + slave receive 32bits");
+    let mut i2s2_transfer = transfer_base_conf.transmit().i2s_transfer(i2s2);
+    let mut i2s3_transfer = transfer_base_conf.to_slave().receive().i2s_transfer(i2s3);
     test_32bits!(i2s2_transfer, i2s3_transfer);
-    loop{}
+    let i2s2 = i2s2_transfer.release();
+    let i2s3 = i2s3_transfer.release();
+    unsafe {
+        let rcc = &(*RCC::ptr());
+        SPI2::reset(rcc);
+        SPI3::reset(rcc);
+    }
+
+    rprintln!("slave transmit + master receive 32bits");
+    let mut i2s2_transfer = transfer_base_conf.to_slave().transmit().i2s_transfer(i2s2);
+    let mut i2s3_transfer = transfer_base_conf.receive().i2s_transfer(i2s3);
+    i2s2_transfer.begin();
+    test_32bits!(i2s2_transfer, i2s3_transfer);
+    let i2s2 = i2s2_transfer.release();
+    let i2s3 = i2s3_transfer.release();
+    unsafe {
+        let rcc = &(*RCC::ptr());
+        SPI2::reset(rcc);
+        SPI3::reset(rcc);
+    }
+
+    rprintln!("slave transmit + slave receive 32bits (external master)");
+    let mut i2s5_driver = driver_base_conf.i2s_driver(i2s5);
+    let mut i2s2_transfer = transfer_base_conf.to_slave().transmit().i2s_transfer(i2s2);
+    let mut i2s3_transfer = transfer_base_conf.to_slave().receive().i2s_transfer(i2s3);
+    i2s2_transfer.begin();
+    i2s3_transfer.begin();
+    i2s5_driver.enable();
+    test_32bits!(i2s2_transfer, i2s3_transfer);
+    let i2s2 = i2s2_transfer.release();
+    let i2s3 = i2s3_transfer.release();
+    unsafe {
+        let rcc = &(*RCC::ptr());
+        SPI2::reset(rcc);
+        SPI3::reset(rcc);
+    }
+
+    loop {}
 }
 
 use core::panic::PanicInfo;
